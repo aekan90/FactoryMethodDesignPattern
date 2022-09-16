1179d58dabff37e6b37f171a281c42a19b5a7890
Atıf Ertugrul Kan <aekan90@hotmail.com>
2022-09-16 12:14:41 +0300

    .

--- FactoryMethodDesignPattern/FactoryMethodDesignPattern/Program.cs
+++ FactoryMethodDesignPattern/FactoryMethodDesignPattern/Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+namespace FactoryMethodDesignMethod
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Creater creater = new Creater();
+            Oyun atariOyunu = creater.FactoryMethod(Oyunlar.Atari);
+            Oyun pcOyunu = creater.FactoryMethod(Oyunlar.PC);
+            Oyun psOyunu = creater.FactoryMethod(Oyunlar.PS);
+
+            atariOyunu.Platform();
+            pcOyunu.Platform();
+            psOyunu.Platform();
+
+            Console.Read();
+        }
+    }
+
+    class Creater
+    // class Creater<Oyun> where Oyun : Enum
+    {
+
+        public Oyun FactoryMethod(Oyunlar OyunTipi)
+        {
+            Oyun oyun = null;
+            switch (OyunTipi)
+            {
+                case Oyunlar.Atari:
+                    oyun = new Atari();
+                    break;
+                case Oyunlar.PC:
+                    oyun = new PC();
+                    break;
+                case Oyunlar.PS:
+                    oyun = new PS();
+                    break;
+            }
+            return oyun;
+        }
+    }
+
+    enum Oyunlar
+    {
+        Atari,
+        PC,
+        PS
+    }
+
+    abstract class Oyun
+    {
+        public abstract void Platform();
+    }
+
+    class Atari : Oyun
+    {
+        public override void Platform()
+        {
+            Console.WriteLine("Bu oyun ATARİ platformunda çalışmaktadır.");
+        }
+    }
+
+    class PC : Oyun
+    {
+        public override void Platform()
+        {
+            Console.WriteLine("Bu oyun PC platformunda çalışmaktadır.");
+        }
+    }
+
+    class PS : Oyun
+    {
+        public override void Platform()
+        {
+            Console.WriteLine("Bu oyun PS platformunda çalışmaktadır.");
+        }
+    }
+}
